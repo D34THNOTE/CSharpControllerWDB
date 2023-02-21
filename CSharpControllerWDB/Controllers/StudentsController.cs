8db03db7dc4ed11ebdc7f198cdf91a309177f38b
Bartosz Janowski <detnotatka@gmail.com>
2023-02-21 18:35:28 +0100

    Students controller complete

--- CSharpControllerWDB/Controllers/StudentsController.cs
+++ CSharpControllerWDB/Controllers/StudentsController.cs
@@ -154,77 +154,135 @@ namespace CSharpControllerWDB.Controllers
             {
                 return BadRequest(ModelState);
             }
-
-            // Create the connection to the database
-            using var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s23375;Integrated Security=True");
-            // This query returns the number of rows in Student that much IndexNumber value specified as the parameter
-            using var command = new SqlCommand("SELECT COUNT(*) FROM Student WHERE IndexNumber = @id", connection);
-            command.Parameters.AddWithValue("@id", studentData.IndexNumber);
-
-            try
-            {
-                connection.Open();
-                int count = (int)command.ExecuteScalar();
-                if (count > 0)
-                {
-                    return BadRequest("A student with given index number already exists");
-                }
-                
-                // Basic SqlCommand methods:
-                // ExecuteNonQuery: Used for executing SQL commands that do not return any result set, like INSERT, UPDATE, or DELETE.
-                // ExecuteScalar: Used for executing SQL commands that return a single value, such as COUNT(*), SUM(column), or MAX(column).
-                // ExecuteReader: Used for executing SQL commands that return a result set, and allows you to read the results row by row.
             
-                using var insertCommand =
-                    new SqlCommand(
+            using (var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s23375;Integrated Security=True"))
+            {
+                try 
+                { 
+                    connection.Open();
+                    
+                    // This query returns the number of rows in Student that much IndexNumber value specified as the parameter
+                    var command = new SqlCommand("SELECT COUNT(*) FROM Student WHERE IndexNumber = @id", connection);
+                    command.Parameters.AddWithValue("@id", studentData.IndexNumber);
+                    int count;
+                    
+                    // (I wrapped every command in its own try-catch block for easier error handling, the try-catch block wrapping the content of "using" statement is sufficient)
+                    try
+                    {
+                        count = (int)command.ExecuteScalar();
+                    }
+                    catch (Exception ex) 
+                    { 
+                        Console.WriteLine(ex); 
+                        return StatusCode(500, "There was an error when verifying the uniqueness of the provided index number"); 
+                    }
+                    command.Dispose();
+                     
+                    if (count > 0) 
+                    { 
+                        return BadRequest("A student with given index number already exists"); 
+                    }
+                    // Basic SqlCommand methods:
+                    // ExecuteNonQuery: Used for executing SQL commands that do not return any result set, like INSERT, UPDATE, or DELETE.
+                    // ExecuteScalar: Used for executing SQL commands that return a single value, such as COUNT(*), SUM(column), or MAX(column).
+                    // ExecuteReader: Used for executing SQL commands that return a result set, and allows you to read the results row by row.
+                    
+                    var insertCommand = 
+                        new SqlCommand(
                         "INSERT INTO Student (IndexNumber, FirstName, LastName, BirthDate, IdEnrollment) VALUES " +
-                        "(@id, @firstname, @lastname, @birthdate, @idenrollment)");
-            
-                insertCommand.Parameters.AddWithValue("@id", studentData.IndexNumber);
-                insertCommand.Parameters.AddWithValue("@firstname", studentData.FirstName);
-                insertCommand.Parameters.AddWithValue("@lastname", studentData.LastName);
-                insertCommand.Parameters.AddWithValue("@birthdate", studentData.BirthDate);
-                insertCommand.Parameters.AddWithValue("@idenrollment", studentData.IdEnrollment);
-
-                try
-                {
-                    insertCommand.ExecuteNonQuery();
+                        "(@id, @firstname, @lastname, @birthdate, @idenrollment)", connection);
+                    
+                    insertCommand.Parameters.AddWithValue("@id", studentData.IndexNumber); 
+                    insertCommand.Parameters.AddWithValue("@firstname", studentData.FirstName); 
+                    insertCommand.Parameters.AddWithValue("@lastname", studentData.LastName); 
+                    insertCommand.Parameters.AddWithValue("@birthdate", studentData.BirthDate); 
+                    insertCommand.Parameters.AddWithValue("@idenrollment", studentData.IdEnrollment);
+                    
+                    try 
+                    { 
+                        insertCommand.ExecuteNonQuery(); 
+                    }
+                    catch (Exception ex) 
+                    { 
+                        Console.WriteLine(ex); 
+                        return StatusCode(500, "There was an error when trying to insert the new record"); 
+                    }
+                    insertCommand.Dispose();
+                    
+                    return Ok(studentData); 
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    return StatusCode(500, "An internal error occured while processing your request");
+                catch (Exception e) 
+                { 
+                    Console.WriteLine(e); 
+                    return StatusCode(500, "An internal error occured while processing your request"); 
                 }
-
-                return Ok(studentData);
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return StatusCode(500, "An internal error occured while processing your request");
-            }
-            
         }
 
-        /*
+        
          
         [HttpPut("{id}")]
-        public ActionResult<Student> Update(string id, [FromBody] Student updatedStudent)
-        {
-            var validationError = ValidateIdFormat(id);
-            if (validationError != null)
-            {
-                return validationError;
+        public ActionResult<Student> Update(string id, [FromBody] Student updatedStudent) 
+        { 
+            var validationError = ValidateIdFormat(id); 
+            if (validationError != null) 
+            { 
+                return validationError; 
             }
+            
+            using (var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s23375;Integrated Security=True")) 
+            { 
+                try 
+                { 
+                    connection.Open();
 
-            // UpdateStudent returns a false if no match was found and true after overwriting the .csv file
-            bool success = CsvDataHandler.UpdateStudent(updatedStudent);
-            if (!success)
-            {
-                return BadRequest("There was no match for the provided student, update failed");
-            }
+                    // Check if student with the given ID exists in the database
 
-            return Ok(updatedStudent);
+                    var checkCommand = new SqlCommand("SELECT COUNT(*) FROM Student WHERE IndexNumber = @id", connection); 
+                    checkCommand.Parameters.AddWithValue("@id", id);
+                    int count;
+                    try
+                    {
+                        count = (int)checkCommand.ExecuteScalar(); 
+                    }
+                    catch (Exception ex) 
+                    { 
+                        Console.WriteLine(ex); 
+                        return StatusCode(500, "There was an error verifying the uniqueness of the provided index number"); 
+                    }
+                    
+                    if (count == 0) 
+                    { 
+                        return NotFound($"Student with ID {id} not found"); 
+                    }
+                    checkCommand.Dispose();
+
+                    // Update the student record in the database
+                    var updateCommand = new SqlCommand(
+                        "UPDATE Student SET FirstName = @firstname, LastName = @lastname, " + 
+                        "BirthDate = @birthdate, IdEnrollment = @idenrollment WHERE IndexNumber = @id", 
+                        connection); 
+                    updateCommand.Parameters.AddWithValue("@id", id); 
+                    updateCommand.Parameters.AddWithValue("@firstname", updatedStudent.FirstName); 
+                    updateCommand.Parameters.AddWithValue("@lastname", updatedStudent.LastName); 
+                    updateCommand.Parameters.AddWithValue("@birthdate", updatedStudent.BirthDate); 
+                    updateCommand.Parameters.AddWithValue("@idenrollment", updatedStudent.IdEnrollment); 
+                    int rowsAffected = updateCommand.ExecuteNonQuery();
+                    
+                    if (rowsAffected == 0) 
+                    { 
+                        return BadRequest("Update failed, no rows affected"); 
+                    }
+                    updateCommand.Dispose();
+                    
+                    return Ok(updatedStudent); 
+                }
+                catch (Exception e) 
+                { 
+                    Console.WriteLine(e); 
+                    return StatusCode(500, "An internal error occured while processing your request"); 
+                } 
+            } 
         }
 
         [HttpDelete("{id}")]
@@ -236,22 +294,69 @@ namespace CSharpControllerWDB.Controllers
                 return validationError;
             }
 
-            var deleted = CsvDataHandler.DeleteStudent(id);
+            using (var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s23375;Integrated Security=True"))
+            {
+                try
+                {
+                    connection.Open();
 
-            if(deleted)
-            {
-                return Ok($"Student with ID {id} successfully deleted");
-            } 
-            else
-            {
-                return NotFound($"Student with ID {id} not found");
+                    // I read that it's a good practice to use BeginTransaction() in Delete methods for data integrity - it helps ensure that in case of an error all statements
+                    // defined here are rolled back so no data inconsistency will occur
+                    using (var transaction = connection.BeginTransaction())
+                    {
+                        try
+                        {
+                            using (var command = new SqlCommand("SELECT COUNT(*) FROM Student WHERE IndexNumber = @id", connection, transaction))
+                            {
+                                command.Parameters.AddWithValue("@id", id);
+                                int count;
+                                try
+                                {
+                                    count = (int)command.ExecuteScalar();
+                                }
+                                catch (Exception ex) 
+                                { 
+                                    Console.WriteLine(ex); 
+                                    return StatusCode(500, "There was an error when verifying the uniqueness of the provided index number"); 
+                                }
+
+                                if (count == 0)
+                                {
+                                    return NotFound("No student with the provided ID was found");
+                                }
+                            }
+                            
+                            using (var command = new SqlCommand("DELETE FROM Student WHERE IndexNumber = @id", connection, transaction))
+                            {
+                                command.Parameters.AddWithValue("@id", id);
+                                try
+                                {
+                                    command.ExecuteNonQuery();
+                                }
+                                catch (Exception ex) 
+                                { 
+                                    Console.WriteLine(ex); 
+                                    return StatusCode(500, "An error occured when deleting the student, no rows were affected"); 
+                                }
+                            }
+
+                            transaction.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            transaction.Rollback();
+                            Console.WriteLine(ex);
+                            return StatusCode(500, "An internal error occured while processing your request");
+                        }
+                    } 
+                }
+                catch (Exception e) 
+                { 
+                    Console.WriteLine(e); 
+                    return StatusCode(500, "An internal error occured while processing your request"); 
+                }
             }
+            return Ok($"Student with ID {id} successfully deleted");
         }
-
-        public StudentsController()
-        {
-
-        }
-        */
     }
 }
